3ac9cfb6c9fdf5abd742d92693a13664fe1ff825
pokedev <pokemondev@outlook.com>
2019-10-16 22:45:16 +0100

    Fixed tab vs space setup

--- src/appServices.ts
+++ src/appServices.ts
@@ -2,7 +2,7 @@ import { DexService } from "./pokemon/dexService";
 import { StatsService } from "./ps-stats/statsService";
 
 export class AppServices {
-  // other singleton services (or factories) might be placed here as well
-  public dexService = new DexService();
-  public statsService = new StatsService();
+	// other singleton services (or factories) might be placed here as well
+	public dexService = new DexService();
+	public statsService = new StatsService();
 }
--- src/pokemon/dexService.ts
+++ src/pokemon/dexService.ts
@@ -4,36 +4,36 @@ import { Pokemon, Dex } from "./models";
 
 export class DexService {
 
-  private pokemonMap: { [name: string]: Pokemon } = {};
-  private dex: Dex = {} as Dex;
-  private fuzzyMatching: FuzzyMatching;
+	private pokemonMap: { [name: string]: Pokemon } = {};
+	private dex: Dex = {} as Dex;
+	private fuzzyMatching: FuzzyMatching;
 
-  constructor() {
-    this.loadFileData();
-    this.fuzzyMatching = new FuzzyMatching(this.dex.pokemon.map(p => p.name));
-  }
+	constructor() {
+		this.loadFileData();
+		this.fuzzyMatching = new FuzzyMatching(this.dex.pokemon.map(p => p.name));
+	}
 
-  public getPokemon(name: string): Pokemon | undefined {
-    const pokemon = this.pokemonMap[name.toLowerCase()];
-    if (pokemon)
-      return pokemon;
+	public getPokemon(name: string): Pokemon | undefined {
+		const pokemon = this.pokemonMap[name.toLowerCase()];
+		if (pokemon)
+			return pokemon;
 
-    const match = this.fuzzyMatching.get(name, undefined);
-    return (match.distance >= 0.5)
-      ? this.pokemonMap[match.value.toLowerCase()]
-      : undefined;
-  }
+		const match = this.fuzzyMatching.get(name, undefined);
+		return (match.distance >= 0.5)
+			? this.pokemonMap[match.value.toLowerCase()]
+			: undefined;
+	}
 
-  private loadFileData(): void {
-    this.dex = this.loadFile<Dex>("dex.json");
-    this.dex.pokemon.forEach(i => {
-      this.pokemonMap[i.name.toLowerCase()] = i;
-    })
-  }
+	private loadFileData(): void {
+		this.dex = this.loadFile<Dex>("dex.json");
+		this.dex.pokemon.forEach(i => {
+			this.pokemonMap[i.name.toLowerCase()] = i;
+		})
+	}
 
-  private loadFile<T>(filename: string): T {
-    const rawdata = fs.readFileSync(`data/${filename}`).toString();
-    const data:T = JSON.parse(rawdata);
-    return data;
-  }
+	private loadFile<T>(filename: string): T {
+		const rawdata = fs.readFileSync(`data/${filename}`).toString();
+		const data:T = JSON.parse(rawdata);
+		return data;
+	}
 }
