e79f427956fa280a5b0dc721d8ead64ea509ddb7
pokedev <pokemondev@outlook.com>
2019-12-03 23:00:02 +0000

    Applyed changes suggested in the code review

--- src/ps-stats/statsService.ts
+++ src/ps-stats/statsService.ts
@@ -6,6 +6,11 @@ enum StatsType { Leads = "leads" }
 
 export class StatsService {
 	
+	/**
+	 * Multi-dimensional dictionary that holds stats data. 
+	 * First dimension is the Stats Type (leads, moves, items, movesets...)
+	 * Second dimension is the format (gen8-ou, gen7-uu...)
+	*/ 
 	private database: { [id: string] : DbData; } = {};
 	
 	public getLeads(format: SmogonFormat): PokemonUsage[] {
@@ -26,14 +31,19 @@ export class StatsService {
 		return this.database[StatsType.Leads][fmt];
 	}
 
+	/** 
+	 * Method responsible to load given format (gen8-ou, gen7-uu) of a given stats type (leads, moves, items) into memory.
+	 * The argument `callback` is used for whatever data transformation the given StatsType need to apply on the real Showdown Stats.
+	*/		
 	private ensureDataIsLoaded(statsType: string, format: SmogonFormat, callback?: (data: any) => any): void {
 		const fmt = this.getFormatKey(format);
 		const dataLoaded = this.database[statsType] && this.database[statsType][fmt];
 		if (!dataLoaded) {
 			let fileData = this.loadFileData(statsType, format);
 
-			if (callback)
+			if (callback) {
 				fileData = callback(fileData);
+			}
 			
 			const data = this.database[statsType] || {} as DbData;
 			data[fmt] = fileData;
